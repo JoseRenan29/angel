8f72a2afa89e90f5a7148395960ae45a9e0139a2
JR <gasidan07@gmail.com>
2023-11-11 00:01:33 +0800

    angel Copy

---
+++ app/src/main/java/com/example/aistillo_android_version/Login_Register_Page.java
@@ -0,0 +1,14 @@
+package com.example.aistillo_android_version;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Register_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register_page);
+    }
+}
---
+++ app/src/main/java/com/example/aistillo_android_version/Register.java
@@ -0,0 +1,14 @@
+package com.example.aistillo_android_version;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
